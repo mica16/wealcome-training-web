190dc51ed46f75180aa9aeaa4108b94853a3cb8a
Michaël AZERHAD <mazerhad@wealcomecompany.com>
2019-05-08 22:23:36 +0200

    pass some UI tests + integration tests

--- src/adapters/secondaries/gateways/httpApartmentsGateway.ispec.ts
+++ src/adapters/secondaries/gateways/httpApartmentsGateway.ispec.ts
@@ -5,7 +5,6 @@ describe('Http Apartments gateway', () => {
     it('should retrieve real data', done => {
         new HttpApartmentsGateway().retrieve()
             .subscribe(apartments => {
-                console.log("yoo");
                 expect(apartments)
                     .toEqual(["53 avenue des Champs-Elysées 75008 Paris"]);
                 done();
